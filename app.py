79495e24cfe7681f6187537ad75c208489f041ef
Dr. Janardanan Sreekumar <sreejyothi_in@yahoo.com>
2025-11-09 20:20:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 #from ensemble_predict import predict_ensemble, class_names
 import sys
 
-#st.write("Python Version:", sys.version)
+st.write("Python Version:", sys.version)
 
 # MobileNetV3 structure
 def get_mobilenet_v3(num_classes=5):
